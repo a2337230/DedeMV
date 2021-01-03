6fdaaceb14d77c1b51db495cfbf203ce6c9697ec
a2337230 <hxfg163com@163.com>
2021-01-03 19:37:48 +0800

    22

---
+++ bd/bdtj_m.php
@@ -0,0 +1,16 @@
+<?php
+$str = $_POST['url'];
+$urls = explode(" ",$str);
+$api = 'http://data.zz.baidu.com/urls?site=m.lookmeinv.com&token=tnAydu2gTaUtOM0l';
+$ch = curl_init();
+$options =  array(
+    CURLOPT_URL => $api,
+    CURLOPT_POST => true,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POSTFIELDS => implode("\n", $urls),
+    CURLOPT_HTTPHEADER => array('Content-Type: text/plain'),
+);
+curl_setopt_array($ch, $options);
+$result = curl_exec($ch);
+echo $result;
+?>
--- data/cache/listcache/62/2a/71/622a71ad89f9d72fadfff9395cc41f2b.php
+++ data/cache/listcache/62/2a/71/622a71ad89f9d72fadfff9395cc41f2b.php
@@ -1,2 +1,2 @@
 <?php exit('dedecms');?>
-a:2:{s:4:"data";a:1:{s:2:"dd";s:2:"11";}s:7:"timeout";i:1577010454;}
+a:2:{s:4:"data";a:1:{s:2:"dd";s:2:"56";}s:7:"timeout";i:1609651682;}
